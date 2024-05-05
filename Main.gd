e04c857d88f3ee52c0c00233830afd214b6ade4b
ghartemann <g.hartemann@gmail.com>
2024-05-05 10:34:35 +0200

    save

--- Main.gd
+++ Main.gd
@@ -1,22 +1,47 @@
 extends Node2D
 
+var update_interval = 0.05
+
 var bananas = 0
+var bps = 1
+var bpc = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var button = Button.new()
-	button.text = "Click me"
-	button.pressed.connect(self._click)
-	add_child(button)
-	$Label.text = str(bananas)
-	$Label.show()
-
+	_start_auto_increment()
+	_update_text()
+	$ButtonMain.text = 'Clique'
+	$ButtonMain.pressed.connect(self._increment_via_bpc)
+	$ButtonMain.show()
+	
+	$LabelBananas.show()
+	$LabelBps.text = 'Bananes par seconde : ' + str(bps)
+	$LabelBpc.text = 'Bananes par clic : ' + str(bpc)
+	$LabelBps.show()
+	$LabelBpc.show()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+# func _process(delta):
+# 	pass
 
-func _click():
-	bananas = bananas + 1
-	print(bananas)
-	$Label.text = str(bananas)
+func _increment_bananas(amount):
+	bananas = bananas + amount
+	_update_text()
+
+func _increment_via_bps():
+	_increment_bananas(bps * update_interval)
+	
+func _increment_via_bpc():
+	_increment_bananas(bpc)
+
+func _update_text():
+	var text = 'Bananes : ' + str(snapped(bananas, 1))
+	$LabelBananas.text = text
+
+func _start_auto_increment():
+	$ScoreTimer.wait_time = update_interval
+	$ScoreTimer.start()
+	$ScoreTimer.connect("timeout", _increment_via_bps)
+
+
